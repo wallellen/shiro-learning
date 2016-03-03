a2fcc8a2c78ae041318fb03b54dec949591618b4
wallellen <wallellen@hotmail.com>
2016-03-03 22:19:49 +0800

    init tutorial maven

---
+++ shiro-tutorial/src/main/java/com/wallellen/sso/shiro/tutorial/Tutorial.java
@@ -0,0 +1,18 @@
+package com.wallellen.sso.shiro.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by walle on 3/3/16.
+ */
+public class Tutorial {
+    private static final transient Logger log = LoggerFactory.getLogger(Tutorial.class);
+
+    public static void main(String[] args) {
+        log.info("My first Apache shiro application");
+        //System.exit(-1);
+
+        log.info("done");
+    }
+}
